7e7da34b3d4837177e1276e5ccc3132beb041dcd
Marissa Salcido <salcidom@uci.edu>
2020-03-03 17:30:44 -0800

    Created the profile fragment design - will change color palette

--- app/src/main/java/com/example/fitme/FoodDiaryFragment.java
+++ app/src/main/java/com/example/fitme/FoodDiaryFragment.java
@@ -3,9 +3,12 @@ package com.example.fitme;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,12 +23,13 @@ public class FoodDiaryFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_food_diary, container, false);
     }
+
+
 
 }
--- app/src/main/java/com/example/fitme/ProfileFragment.java
+++ app/src/main/java/com/example/fitme/ProfileFragment.java
@@ -25,6 +25,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
