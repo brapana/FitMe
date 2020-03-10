5c0fc57669b5f1f0d894d9eb723721cd401bfdd0
Marissa Salcido <salcidom@uci.edu>
2020-03-09 23:45:15 -0700

    Fixed Food Diary fragment styling issues Added Add Food fragment

--- app/src/main/java/com/example/fitme/AddEventFragment.java
+++ app/src/main/java/com/example/fitme/AddEventFragment.java
@@ -3,18 +3,21 @@ package com.example.fitme;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddEventFragment extends Fragment {
-
+    private Button btnSubmitEvent;
 
     public AddEventFragment() {
         // Required empty public constructor
@@ -26,6 +29,19 @@ public class AddEventFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_event, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnSubmitEvent = getActivity().findViewById(R.id.btnSubmitEvent);
+        btnSubmitEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).changeFragmentFromFragment(ScheduleFragment.class);
+            }
+        });
+
     }
 
 }
