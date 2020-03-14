9862c4dc607f3fa9c27192a68786647c684ae175
Kevin <kevindlee98@gmail.com>
2020-03-13 18:35:07 -0700

    committing stuff for the favorites page

--- app/src/main/java/com/example/fitme/FavoritesFragment.java
+++ app/src/main/java/com/example/fitme/FavoritesFragment.java
@@ -3,11 +3,17 @@ package com.example.fitme;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +21,10 @@ import android.view.ViewGroup;
  */
 public class FavoritesFragment extends Fragment {
 
+    private RecyclerView rvEx;
+    protected ExAdapter adapterE;
+
+    protected ArrayList<ArrayList<String>> aList = new ArrayList<ArrayList<String>>();
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -25,7 +35,15 @@ public class FavoritesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayoutManager layoutManagerE = new LinearLayoutManager(getContext());
+        rvEx.setLayoutManager(layoutManagerE);
+        rvEx.setAdapter(adapterE);
+    }
 }
